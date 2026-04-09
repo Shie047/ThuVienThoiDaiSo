917fdd896e9b6799c75c719f58084881633135a3
Shie047 <ricute069@gmail.com>
2026-04-10 01:50:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,10 @@ let waitingPlayer = null;
 let roomCounter = 0;
 const rooms = {};
 
-// DỮ LIỆU BOSS THẾ GIỚI (Chạy liên tục trên Server)
-let worldBoss = {
-    hp: 50000,
-    maxHp: 50000,
-    x: 400,
-    isDead: false
-};
-const coopPlayers = {}; // Lưu trữ những ai đang ở trong phòng Boss Thế Giới
+// DỮ LIỆU BOSS THẾ GIỚI
+let worldBoss = { hp: 100000, maxHp: 100000, x: 400, isDead: false };
+const coopPlayers = {}; 
+let coopLobby = {}; // Chứa người đang ở phòng chờ Boss
 
 io.on('connection', (socket) => {
     // === 1v1 MATCHMAKING ===
@@ -25,74 +21,79 @@ io.on('connection', (socket) => {
         if (waitingPlayer && waitingPlayer !== socket) {
             roomCounter++;
             const roomId = 'room_' + roomCounter;
-            
-            // Random Map (0 đến 4) cho 2 người
             const mapIndex = Math.floor(Math.random() * 5); 
 
             socket.join(roomId); waitingPlayer.join(roomId);
-            rooms[roomId] = { p1: waitingPlayer, p2: socket, rematchVotes: 0 };
+            rooms[roomId] = { p1: waitingPlayer, p2: socket };
             
             waitingPlayer.emit('matchFound', { role: 'p1', roomId: roomId, oppName: socket.playerName, mapId: mapIndex });
             socket.emit('matchFound', { role: 'p2', roomId: roomId, oppName: waitingPlayer.playerName, mapId: mapIndex });
             waitingPlayer = null;
         } else {
             waitingPlayer = socket;
-            socket.emit('waiting', 'Đang tìm đối thủ...');
+            socket.emit('waiting_1v1'); // Mở màn hình chờ
         }
     });
+
+    socket.on('cancelMatch', () => { if(waitingPlayer === socket) waitingPlayer = null; });
 
     socket.on('playerAction', (data) => socket.to(data.roomId).emit('updateOpponent', data.playerData));
     socket.on('playerHit', (data) => socket.to(data.roomId).emit('takeDamage', data.damage));
     socket.on('shoot', (data) => socket.to(data.roomId).emit('opponentShoot', data));
+    socket.on('applyStun', (data) => socket.to(data.roomId).emit('takeStun', data.duration));
 
-    // === CHẾ ĐỘ CO-OP (BOSS THẾ GIỚI) ===
-    socket.on('joinWorldBoss', (playerData) => {
-        socket.join('world_boss_room');
-        coopPlayers[socket.id] = playerData;
+    // === CO-OP WORLD BOSS LOBBY ===
+    socket.on('joinWorldBossLobby', (playerData) => {
+        socket.join('wb_lobby');
+        coopLobby[socket.id] = playerData;
+        io.to('wb_lobby').emit('lobbyUpdate', Object.keys(coopLobby).length); // Báo số người
         
-        // Gửi thông tin Boss và danh sách đồng đội cho người mới
-        socket.emit('worldBossInit', { boss: worldBoss, teammates: coopPlayers });
-        // Báo cho những người cũ có người mới vào
-        socket.to('world_boss_room').emit('teammateJoined', { id: socket.id, data: playerData });
+        if(Object.keys(coopLobby).length >= 4) startWorldBoss(); // Đủ 4 tự động vào
     });
+    
+    socket.on('leaveWorldBossLobby', () => {
+        socket.leave('wb_lobby'); delete coopLobby[socket.id];
+        io.to('wb_lobby').emit('lobbyUpdate', Object.keys(coopLobby).length);
+    });
+
+    socket.on('startWorldBossEarly', () => startWorldBoss()); // Ép vào luôn khi chưa đủ
+
+    function startWorldBoss() {
+        const playersInLobby = Object.keys(coopLobby);
+        if(playersInLobby.length === 0) return;
+        
+        for(let id of playersInLobby) {
+            const s = io.sockets.sockets.get(id);
+            if(s) { s.leave('wb_lobby'); s.join('world_boss_room'); coopPlayers[id] = coopLobby[id]; }
+        }
+        coopLobby = {}; 
+        io.to('world_boss_room').emit('worldBossInit', { boss: worldBoss, teammates: coopPlayers });
+    }
 
     socket.on('coopPlayerAction', (data) => {
         if(coopPlayers[socket.id]) {
-            coopPlayers[socket.id] = data;
-            socket.to('world_boss_room').emit('updateTeammate', { id: socket.id, data: data });
+            coopPlayers[socket.id] = data; socket.to('world_boss_room').emit('updateTeammate', { id: socket.id, data: data });
         }
     });
 
-    socket.on('coopShoot', (data) => {
-        socket.to('world_boss_room').emit('teammateShoot', data);
-    });
-
+    socket.on('coopShoot', (data) => socket.to('world_boss_room').emit('teammateShoot', data));
+    socket.on('coopStunBoss', (dur) => socket.to('world_boss_room').emit('bossStunned', dur));
+    
     socket.on('hitWorldBoss', (damage) => {
         if (worldBoss.isDead) return;
         worldBoss.hp -= damage;
-        
-        // Broadcast HP mới cho tất cả
         io.to('world_boss_room').emit('worldBossHpUpdate', worldBoss.hp);
 
         if (worldBoss.hp <= 0) {
-            worldBoss.isDead = true;
-            io.to('world_boss_room').emit('worldBossDefeated');
-            
-            // Hồi sinh Boss sau 30 giây
-            setTimeout(() => {
-                worldBoss.hp = worldBoss.maxHp;
-                worldBoss.isDead = false;
-                io.to('world_boss_room').emit('worldBossRespawn');
-            }, 30000);
+            worldBoss.isDead = true; io.to('world_boss_room').emit('worldBossDefeated');
+            setTimeout(() => { worldBoss.hp = worldBoss.maxHp; worldBoss.isDead = false; }, 30000);
         }
     });
 
     socket.on('disconnect', () => { 
         if (waitingPlayer === socket) waitingPlayer = null; 
-        if (coopPlayers[socket.id]) {
-            delete coopPlayers[socket.id];
-            io.to('world_boss_room').emit('teammateLeft', socket.id);
-        }
+        if (coopLobby[socket.id]) { delete coopLobby[socket.id]; io.to('wb_lobby').emit('lobbyUpdate', Object.keys(coopLobby).length); }
+        if (coopPlayers[socket.id]) { delete coopPlayers[socket.id]; io.to('world_boss_room').emit('teammateLeft', socket.id); }
     });
 });
 
